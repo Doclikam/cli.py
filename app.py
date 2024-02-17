764cef23c073de419f75b05929968bbf4833be6e
Doclikam <likamwambui28@gmail.com>
2024-02-17 13:12:16 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('Hello world')
+def temp_converter(temp_from, temp_to, temp_value):
+    if (temp_from=='FARENHEIGHT' and temp_to=='DEGREE'):
+        temp=temp_value- 33.5
+        print(f"{temp_value}ferenheight is equal to{temp}in degree celcius!")
+    elif (temp_from=='DEGREE' and temp_to=='FARENHEIGHT'):
+        temp=temp_value + 33.5
+        print(f"{temp_value} in degree celcius is equal to{temp} in farenheight!")
+        
+    
+
+temp_from=input("Are you converting from 'farenheight' or 'degrees':").upper()
+temp_to=input("Are you converting from 'farenheight' or 'degrees': ").upper()
+temp_value=float(input('enter the temperature value: '))
+
+temp_converter(temp_from, temp_to, temp_value)
+
+
+
--- mar.py
+++ mar.py
@@ -1,1 +1,2 @@
 print('closing')
+print('add')
